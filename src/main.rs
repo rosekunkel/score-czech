282d11e31f11490e0df5f15d9ffc3c98cd1c81da
William Kunkel <will@wkunkel.com>
2016-12-23 01:21:12 -0500

    Added randomness.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate iron;
 
 #[macro_use(router)]
 extern crate router;
+extern crate rand;
 
 use iron::prelude::*;
 use iron::status;
@@ -10,7 +11,7 @@ fn main() {
     let router = router!(random: get "/random" => handle_random);
 
     fn handle_random(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "A random Czech.")))
+        Ok(Response::with((status::Ok, rand::random::<u32>().to_string())))
     }
 
     Iron::new(router).http("localhost:3000").unwrap();
