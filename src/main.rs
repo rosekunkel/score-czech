1261a08d6e82b16dab8afa86852d6dbeba7ecf25
William Kunkel <will@wkunkel.com>
2016-12-23 01:59:28 -0500

    Added Czech struct.

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,47 @@ extern crate iron;
 extern crate router;
 extern crate rand;
 
+use std::fmt;
 use iron::prelude::*;
-use iron::status;
+use iron::{status, headers};
+
+#[derive(Debug)]
+struct Czech<'a> {
+    name: &'a str,
+}
+
+impl<'a> Czech<'a> {
+    fn new(name: &str) -> Czech {
+        Czech {
+            name: name,
+        }
+    }
+}
+
+impl<'a> fmt::Display for Czech<'a> {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "{}", self.name)
+    }
+}
+
+impl<'a> rand::Rand for Czech<'a> {
+    fn rand<R: rand::Rng>(rng: &mut R) -> Czech<'static> {
+        let names = ["Lída Baarová",
+                     "Jan Santini Aichel",
+                     "Michal Ajvaz",
+                     "Ivana Christová"];
+        Czech::new(rng.choose(&names).unwrap())
+    }
+}
 
 fn main() {
     let router = router!(random: get "/random" => handle_random);
 
     fn handle_random(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, rand::random::<u32>().to_string())))
+        let mut response =
+            Response::with((status::Ok, rand::random::<Czech>().to_string()));
+        response.headers.set(headers::ContentType::plaintext());
+        Ok(response)
     }
 
     Iron::new(router).http("localhost:3000").unwrap();
